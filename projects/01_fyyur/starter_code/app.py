be096734cbbc79fe3cf532c11434615bee977c6c
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-06 14:56:28 +0100

    restructured venue list

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -152,12 +152,24 @@ def venues():
         loc_set.add((venue.city, venue.state))
 
     for loc in loc_set:
-        data_Item = {
+        data_item = {
             'city': loc[0],
             'state': loc[1],
-            'venues': venue_qs.filter_by(city=loc[0]).filter_by(state=loc[1]).all(),
+            'venues': [],
         }
-        data.append(data_Item)
+        loc_qs = venue_qs.filter_by(city=loc[0]).filter_by(state=loc[1]).all()
+        print(loc_qs)
+        for venue_loc in loc_qs:
+
+            data_item['venues'].append({
+                'id': venue_loc.id,
+                'name': venue_loc.name,
+                'num_upcoming_shows': Show.query
+                .filter(Show.venue_id == venue_loc.id)
+                .filter(Show.event_date > datetime.now())
+                .count()
+            })
+        data.append(data_item)
 
     return render_template('pages/venues.html', areas=data)
 
@@ -191,46 +203,7 @@ def search_venues():
 def show_venue(venue_id):
     # shows the venue page with the given venue_id
     # TODO: replace with real venue data from the venues table, using venue_id
-    data1 = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        "past_shows": [{
-            "artist_id": 4,
-            "artist_name": "Guns N Petals",
-            "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-            "start_time": "2019-05-21T21:30:00.000Z"
-        }],
-        "upcoming_shows": [],
-        "past_shows_count": 1,
-        "upcoming_shows_count": 0,
-    }
-    data2 = {
-        "id": 2,
-        "name": "The Dueling Pianos Bar",
-        "genres": ["Classical", "R&B", "Hip-Hop"],
-        "address": "335 Delancey Street",
-        "city": "New York",
-        "state": "NY",
-        "phone": "914-003-1132",
-        "website": "https://www.theduelingpianos.com",
-        "facebook_link": "https://www.facebook.com/theduelingpianos",
-        "seeking_talent": False,
-        "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-        "past_shows": [],
-        "upcoming_shows": [],
-        "past_shows_count": 0,
-        "upcoming_shows_count": 0,
-    }
+
     data3 = {
         "id": 3,
         "name": "Park Square Live Music & Coffee",
@@ -268,9 +241,9 @@ def show_venue(venue_id):
         "past_shows_count": 1,
         "upcoming_shows_count": 1,
     }
-    data = list(filter(lambda d: d['id'] ==
-                venue_id, [data1, data2, data3]))[0]
-    return render_template('pages/show_venue.html', venue=data)
+    # data = list(filter(lambda d: d['id'] ==
+    #             venue_id, [data1, data2, data3]))[0]
+    return render_template('pages/show_venue.html', venue=data3)
 
 
 #  Create Venue
