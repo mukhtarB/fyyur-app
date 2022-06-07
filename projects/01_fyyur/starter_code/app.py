11f3d8aef14240bf9f364be89ad608f2c1bb9b63
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-06-07 18:45:21 +0100

    [IMP] POST edit form

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -446,6 +446,26 @@ def edit_artist(artist_id):
 def edit_artist_submission(artist_id):
     # TODO: take values from the form submitted, and update existing
     # artist record with ID <artist_id> using the new attributes
+    try:
+        form = ArtistForm()
+        artist = Artist.query.get(artist_id)
+
+        artist.name = form.name.data
+        artist.genres = ", ".join(form.genres.data)
+        artist.city = form.city.data
+        artist.state = form.state.data
+        artist.phone = form.phone.data
+        artist.website_link = form.website_link.data
+        artist.facebook_link = form.facebook_link.data
+        artist.venue_search = form.seeking_venue.data
+        artist.description = form.seeking_description.data
+        artist.image_link = form.image_link.data
+
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
 
     return redirect(url_for('show_artist', artist_id=artist_id))
 
